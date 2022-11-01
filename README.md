8f3de9b5985d258f86d86fd71f335a038ff38fac
gustavocambareri <gustavo.cambareri@gmail.com>
2022-11-01 21:24:45 +0100

    Change

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # P5-empty-project
+
 This repository is the starting point of the assignments given in the elective course [Creative Coding at Politecnico di Milano](https://www11.ceda.polimi.it/schedaincarico/schedaincarico/controller/scheda_pubblica/SchedaPublic.do?&evn_default=evento&c_classe=696598&__pj0=0&__pj1=3ed8420c42c849845b5caa3de626e8fc).
 Browse [this website](https://drawwithcode.github.io/) if you want to know more about it.
+
+I would like this sketch to be non-looping, and working in a different way everytime I say a different word, in the same way a parrot would answer each time, but maybe some parrots keep saying the same one
--- sketch.js
+++ sketch.js
@@ -1,16 +1,50 @@
-function preload() {
-	// put preload code here
-}
+let parrotImage;
+let parrotSound;
+let myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+let foo = new p5.Speech(); // speech synthesis object
 
+function preload() {
+  //loading assets
+  parrotImage = loadImage("./assets/parrot3.png");
+  parrotSound = loadSound("./assets/khsound.mp3");
+}
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  //starts to take incoming audio
+  myRec.start();
+  fill(255);
 }
 
 function draw() {
-	// put drawing code here
+  background(0);
+
+  image(
+    parrotImage,
+    width / 2 - 210,
+    height / 2 - 210,
+    parrotImage.width / 1.5,
+    parrotImage.height / 1.5
+  );
+
+  // instructions:
+  textSize(32);
+  textAlign(CENTER);
+  //I have to tell the user to click first because if I don't click, the foo object won't speak. I don't know why, but this is a design workaround to solve the problem.
+  //I would like this sketch to be non-looping, and working in a different way everytime I say a different word, but I can't right now. Still a lot to learn about objects and classes
+  text("click first and then speak to me", width / 2 - 300, height / 2);
+  parrotTalk();
+}
+function mouseClicked() {
+  parrotSound.play();
+}
+
+function parrotTalk() {
+  //the if true condition means if the recording has ended succesfully
+  if (myRec.resultValue == true) {
+    //resultString is a property of the library which contains the string of what you said
+    console.log(myRec.resultString);
+    foo.speak(myRec.resultString);
+    fill(255);
+    text("I won't stop", width / 2 - 300, height / 2 + 300);
+  }
 }
